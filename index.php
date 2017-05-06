9dd60ad4d513a055b2e35d7e88bab474d2b9c8af
Henry Leon <Helg18@gmail.com>
2017-05-05 20:42:40 -0400

    Commit inicial

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/bootstrap.min.css" type="text/css" rel="stylesheet">
+	<link href="css/bootstrap.css" type="text/css" rel="stylesheet">
+	<link href="css/estilos.css" type="text/css" rel="stylesheet">
+	<title>Inicio - Plantilla</title>
+</head>
+<body>
+<div class="container">
+	<div class="row-fluid">
+		<div class="span12">
+		<!-- Comienza el encabezado -->
+			<nav class="navbar navbar-default" role="navigation">
+			  <div class="container-fluid">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Menu de navegaci&oacute;n</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand active" href="index.php"><i class="glyphicon glyphicon-home"></i> Inicio </a>
+					</div>
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav">
+							<li><a href="#">Conocenos</i></a></li>
+							<li><a href="#">Servicios</a></li>
+							<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Productos <span class="caret"></span></a>
+								<ul class="dropdown-menu" role="menu">
+									<li><a href="#">Producto 1</a></li>
+									<li><a href="#">Producto 2</a></li>
+									<li><a href="#">Producto 3</a></li>
+									<li class="divider"></li>
+									<li><a href="#">Software a la medida</a></li>
+									<li class="divider"></li>
+									<li><a href="#">Dise&ntilde;o Web</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Contactanos</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+			  </div><!-- /.container-fluid -->
+			</nav>
+		</div><!--Div span12  -->
+	</div><!-- div row-fluid  -->
+<!--Fin encabezado -->
+
+<!-- Aqui esta el cuerpo del sitio web   -->
+<!-- Comienza carrussel -->
+<div class="row">
+	<div align="center">
+		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		  
+		  <!-- Indicators -->
+		  <ol class="carousel-indicators">
+		    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		  </ol>
+
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner">
+		    <div class="item active">
+		      <img src="img/slide1.jpg" alt="...">
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="img/slide2.jpg" alt="...">
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		    <div class="item">
+		      <img src="img/slide3.jpg" alt="...">
+		      <div class="carousel-caption">
+		        ...
+		      </div>
+		    </div>
+		  </div>
+
+		  <div class="span12" align="center">
+		  	<strong>Nombre del cliente plantilla free</strong>
+		  </div>
+
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left"></span>
+		  </a>
+		  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right"></span>
+		  </a>
+
+		</div>
+	</div>
+</div>
+
+<!-- termina carrussel -->
+
+<div class="row">
+		<div class="col-xs-4" align="center"><br><br>
+		  <img src="img/sinimagen.jpg" alt="Texto de imagen" class="img-rounded" align="center">
+		  <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, 
+		  perferendis ipsam pariatur, beatae in nesciunt praesentium eum. Lorem ipsum dolor sit amet, 
+		  consectetur adipisicing elit. Illo praesentium, aliquid. Minus fugit veniam, perspiciatis. 
+		  Accusantium vitae itaque ipsam animi officiis, commodi aperiam iusto beatae nam dolorem ex, 
+		  saepe architecto.</p>
+ 		</div>
+
+    <div class="col-xs-4" align="center"><br><br>
+		  <img src="img/sinimagen.jpg" alt="Texto de imagen" class="img-rounded" align="center">
+		  <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, 
+		  perferendis ipsam pariatur, beatae in nesciunt praesentium eum. Lorem ipsum dolor sit amet, 
+		  consectetur adipisicing elit. Illo praesentium, aliquid. Minus fugit veniam, perspiciatis. 
+		  Accusantium vitae itaque ipsam animi officiis, commodi aperiam iusto beatae nam dolorem ex, 
+		  saepe architecto.</p>
+ 		</div>
+
+ 		<div class="col-xs-4" align="center"><br><br>
+		  <img src="img/sinimagen.jpg" alt="Texto de imagen" class="img-rounded" align="center">
+		  <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, 
+		  perferendis ipsam pariatur, beatae in nesciunt praesentium eum. Lorem ipsum dolor sit amet, 
+		  consectetur adipisicing elit. Tempora repudiandae dolor at, necessitatibus ratione animi 
+		  laudantium aspernatur illum commodi inventore, nemo ad voluptate est in molestias, asperiores 
+		  pariatur.</p>
+ 		</div><br>
+<br><br><br>
+    <div class="col-xs-3" align="center"><br>
+	    <img src="img/index-info.gif" alt="Texto de imagen" class="img-rounded" align="center">
+	    <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, perferendis ipsam pariatur, beatae in nesciunt praesentium eum.</p>
+    </div>
+    <div class="col-xs-3" align="center"><br>
+	    <img src="img/index-info.gif" alt="Texto de imagen" class="img-rounded" align="center">
+	    <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, perferendis ipsam pariatur, beatae in nesciunt praesentium eum.</p>
+    </div>
+    <div class="col-xs-3" align="center"><br>
+	    <img src="img/index-info.gif" alt="Texto de imagen" class="img-rounded" align="center">
+	    <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, perferendis ipsam pariatur, beatae in nesciunt praesentium eum.</p>
+    </div>
+    <div class="col-xs-3" align="center"><br>
+	    <img src="img/index-info.gif" alt="Texto de imagen" class="img-rounded" align="center">
+	    <p align="justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam fuga distinctio minima, perferendis ipsam pariatur, beatae in nesciunt praesentium eum.</p>
+    </div>
+</div>
+
+</div><!--dv Container principal  -->
+<!-- termina aqui el cuerpo del sitio web-->
+
+<!-- Comienza footer  -->
+<footer >
+	<div class="row">
+    <div class="col-xs-4 blockfooter">
+    <p class="bfd">Ubicanos</p>
+		Av. principal C.C. El unico, PB<br>
+		local 11-12 Maracaibo, Zulia.<br>
+		Venezuela.<br>
+		</div>
+
+    <div class="col-xs-4 blockfooter">
+    <p class="bfd">Contactanos</p>
+    <strong>Telefonos: </strong>0261-123.45.67<br>
+    <strong>Telefax: </strong>0261-123.45.67<br>
+    <strong>Correo: </strong> correo@dominio.com<br>
+    </div>
+
+    <div class="col-xs-4 blockfooter">
+    <p class="bfd">Redes sociales</p>
+		<a href="#"><img src="img/facebookIcon.jpg" width="10%"></a>
+    </div>
+  </div>
+
+  <div align="center">
+		<nav class="navbar navbar-default" role="navigation">
+					<div><strong>Plantilla free</strong></div>
+					<div>Sitio web desarrollado por: <a href="mailto:helg18@gmail.com" target="_BLANK">Ing. Henry Leon</a></div>
+		</nav>
+  </div>
+</footer>
+
+<!-- Termina footer -->
+
+<!--importar scripts -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<!--Termina de importar scripts -->
+
+</body>
+</html>
